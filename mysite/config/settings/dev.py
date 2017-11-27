7d6ca03d53742147e2c60a32b31c4fdd53f99244
Mark Kim <viking617617@gmail.com>
2017-11-27 21:15:27 +0900

    aws accesskey config_dev로 이동

--- mysite/config/settings/dev.py
+++ mysite/config/settings/dev.py
@@ -3,9 +3,11 @@ from .base import *
 config_secret = json.loads(open(CONFIG_SECRET_DEV_FILE).read())
 
 # AWS
-AWS_ACCESS_KEY_ID = config_secret_common['aws']['access_key_id']
-AWS_SECRET_ACCESS_KEY = config_secret_common['aws']['secret_access_key']
-AWS_STORAGE_BUCKET_NAME = config_secret_common['aws']['s3_bucket_name']
+AWS_ACCESS_KEY_ID = config_secret['aws']['access_key_id']
+AWS_SECRET_ACCESS_KEY = config_secret['aws']['secret_access_key']
+AWS_STORAGE_BUCKET_NAME = config_secret['aws']['s3_bucket_name']
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_REGION_NAME = 'ap-northeast-2'
 
 # AWS Storage
 STATICFILES_LOCATION = 'static'
